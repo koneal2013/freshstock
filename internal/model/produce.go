13b00c269b894ffc83678f28fcd49ab896bced31
Kenston O'Neal <koneal2013@gmail.com>
2023-12-15 00:53:58 -0500

    feat: Refactor API tests and enhance currency handling in produce model

--- internal/model/produce.go
+++ internal/model/produce.go
@@ -1,17 +1,37 @@
 package model
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"regexp"
 
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
+type Money float64
+
+func (m Money) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"$%.2f\"", m)), nil
+}
+
+func (m *Money) UnmarshalJSON(data []byte) error {
+	var num float64
+	if err := json.Unmarshal(data, &num); err != nil {
+		return err
+	}
+
+	// Set the value of m to the float64 value
+	*m = Money(num)
+
+	return nil
+}
+
 type Produce struct {
-	Code      string  `json:"code" binding:"required,code,len=19"`
-	Name      string  `json:"name" binding:"required,alphanum"`
-	UnitPrice float64 `json:"unit_price" binding:"required,gte=0"`
+	Code      string `json:"code" binding:"required,code,len=19"`
+	Name      string `json:"name" binding:"required,alphanum"`
+	UnitPrice Money  `json:"unit_price" binding:"required,gte=0"`
 }
 
 func RegisterValidator() error {
--- test/api_test.go
+++ test/api_test.go
@@ -2,8 +2,6 @@ package test
 
 import (
 	"bytes"
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,15 +16,11 @@ import (
 
 func TestHandlers(t *testing.T) {
 	testStore := store.NewProduceStore()
-	testProduce := model.Produce{
-		Code:      "A12T-4GH7-QPL9-3N4M",
-		Name:      "Lettuce",
-		UnitPrice: 3.46,
-	}
-	testProduceBytes, err := json.Marshal(testProduce)
-	require.NoError(t, err)
+	testProduce := `{"code":"A12T-4GH7-QPL9-3N4M","name":"Lettuce","unit_price":3.46}`
+	expectedResult := `{"code":"A12T-4GH7-QPL9-3N4M","name":"Lettuce","unit_price":"$3.46"}`
+	expectedResultSearch := `[{"code":"A12T-4GH7-QPL9-3N4M","name":"Lettuce","unit_price":"$3.46"}]`
 
-	err = model.RegisterValidator()
+	err := model.RegisterValidator()
 	require.NoError(t, err)
 
 	h := api.NewHandlers(testStore)
@@ -38,14 +32,10 @@ func TestHandlers(t *testing.T) {
 		body   []byte
 		method string
 	}{
-		{name: "add produce", path: "/produce/", body: testProduceBytes,
-			method: http.MethodPost},
-		{name: "get produce by code", path: fmt.Sprintf("/produce/%s", testProduce.Code),
-			method: http.MethodGet},
-		{name: "search produce", path: fmt.Sprintf("/produce/?q=%s", testProduce.Name),
-			method: http.MethodGet},
-		{name: "delete produce", path: fmt.Sprintf("/produce/%s", testProduce.Code),
-			method: http.MethodDelete},
+		{name: "add produce", path: "/produce/", body: []byte(testProduce), method: http.MethodPost},
+		{name: "get produce by code", path: "/produce/A12T-4GH7-QPL9-3N4M", method: http.MethodGet},
+		{name: "search produce", path: "/produce/?q=let", method: http.MethodGet},
+		{name: "delete produce", path: "/produce/A12T-4GH7-QPL9-3N4M", method: http.MethodDelete},
 	}
 
 	for _, test := range tests {
@@ -57,33 +47,16 @@ func TestHandlers(t *testing.T) {
 			switch test.name {
 			case "add produce":
 				assert.Equal(t, http.StatusCreated, w.Code)
-				compareResults(t, testProduce, w)
+				assert.Equal(t, expectedResult, string(w.Body.Bytes()))
 			case "delete produce":
 				assert.Equal(t, http.StatusNoContent, w.Code)
+			case "search produce":
+				assert.Equal(t, http.StatusOK, w.Code)
+				assert.Equal(t, expectedResultSearch, string(w.Body.Bytes()))
 			default:
 				assert.Equal(t, http.StatusOK, w.Code)
-				compareResults(t, testProduce, w)
+				assert.Equal(t, expectedResult, string(w.Body.Bytes()))
 			}
 		})
-	}
-}
-
-func compareResults(t *testing.T, expected model.Produce, rr *httptest.ResponseRecorder) {
-	t.Helper()
-
-	var result model.Produce
-	err := json.Unmarshal(rr.Body.Bytes(), &result)
-	if err != nil {
-		var results []model.Produce
-		err := json.Unmarshal(rr.Body.Bytes(), &results)
-		require.NoError(t, err)
-		assert.Equal(t, 1, len(results))
-		assert.Equal(t, expected.Name, results[0].Name)
-		assert.Equal(t, expected.Code, results[0].Code)
-		assert.Equal(t, expected.UnitPrice, results[0].UnitPrice)
-	} else {
-		assert.Equal(t, expected.Name, result.Name)
-		assert.Equal(t, expected.Code, result.Code)
-		assert.Equal(t, expected.UnitPrice, result.UnitPrice)
 	}
 }
